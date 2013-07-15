c5875fed17116d2cc94e9643ce0cfa89ab2c1886
Dan Gold <danjargold@gmail.com>
2013-07-15 13:13:57 +0000

    web.js improved buffer definition and initialize

--- web.js
+++ web.js
@@ -4,11 +4,9 @@ var fs      = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var htmlBuffer = new Buffer(100);
-    htmlBuffer     = fs.readFileSync('index.html');
+    var htmlBuffer = fs.readFileSync('index.html');
     var htmlStr    = htmlBuffer.toString();
     response.send(htmlStr);
-    response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
